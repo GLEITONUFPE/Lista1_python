d133eb725e50e5eda94d198c2e5e2a9996113963
Gleiton Vilarim <gleitonfreitas@gmail.com>
2023-02-07 23:56:27 -0300

    inclusão de respostas na lista python

---
+++ Q12media.py
@@ -0,0 +1,29 @@
+
+quant = 0
+soma = 0
+resp = 's'
+
+
+
+while resp == 's':
+    num = float(input("Digite um número: "))
+
+    if num > 0:
+        quant += 1 
+        soma += num
+        
+        resp = input("Digite s para continuar: ")
+        
+    else:
+        print("A média dos números é:", soma/quant)
+     
+    
+        
+
+
+
+
+
+
+
+

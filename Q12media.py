f184203ccde54f561c3e3fd0f6bb7c7574e995dc
Gleiton Vilarim <gleitonfreitas@gmail.com>
2023-02-07 23:59:06 -0300

    nova questao resolvida

--- Q12media.py
+++ Q12media.py
@@ -3,27 +3,14 @@ quant = 0
 soma = 0
 resp = 's'
 
-
-
 while resp == 's':
     num = float(input("Digite um número: "))
 
     if num > 0:
-        quant += 1 
+        quant += 1
         soma += num
-        
+
         resp = input("Digite s para continuar: ")
-        
+
     else:
         print("A média dos números é:", soma/quant)
-     
-    
-        
-
-
-
-
-
-
-
-

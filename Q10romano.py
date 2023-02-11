76e514286879be53bd5d4a04f54e46626310210f
Gleiton Vilarim <gleitonfreitas@gmail.com>
2023-02-10 21:41:37 -0300

    novas questoes resolvidas na lista de python

--- Q10romano.py
+++ Q10romano.py
@@ -1,2 +1,29 @@
 
+def para_romano(num):
+    val = [
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4,
+        1
+    ]
+    simb = [
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"
+    ]
+    num_romano = ''
+    i = 0
+    while num > 0:
+        for _ in range(num // val[i]):
+            num_romano += simb[i]
+            num -= val[i]
+        i += 1
+    return num_romano
 
+
+n = int(input("Digite quantos numeros quer converter em romanos: "))
+
+for i in range(n):
+    num = int(input("Digite um número inteiro positivo: "))
+    print(para_romano(num))

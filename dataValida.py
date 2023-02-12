7b8f1d0f7e69d43bb65c32ed4b8e2eb078730445
Gleiton Vilarim <gleitonfreitas@gmail.com>
2023-02-12 14:59:48 -0300

    incluida questao data valida da area tecnica

---
+++ dataValida.py
@@ -0,0 +1,27 @@
+def data_valida(dia, mes, ano):
+    if ano >= 1900 and ano <= 2100:
+        if mes >= 1 and mes <= 12:
+            if mes in [1, 3, 5, 7, 8, 10, 12]:
+                if dia >= 1 and dia <= 31:
+                    return True
+            elif mes in [4, 6, 9, 11]:
+                if dia >= 1 and dia <= 30:
+                    return True
+            elif mes == 2:
+                if ano % 400 == 0 or (ano % 4 == 0 and ano % 100 != 0):
+                    if dia >= 1 and dia <= 29:
+                        return True
+                else:
+                    if dia >= 1 and dia <= 28:
+                        return True
+    return False
+
+
+dia = int(input("Digite o dia: "))
+mes = int(input("Digite o mês: "))
+ano = int(input("Digite o ano: "))
+
+if data_valida(dia, mes, ano):
+    print("valida")
+else:
+    print("invalida")
